8bf0ee2a4fbaeeebc96004e16327e21844ae7545
mawalou14 <awaloum213@gmail.com>
2024-08-02 14:08:56 +0100

    add header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DragDropModule, MatToolbarModule, MatIconModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'dragDropCards';
